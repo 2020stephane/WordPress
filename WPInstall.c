eb767a13ff923a36aad6cbb90565a77bf018f0cf
2020stephane <stephanebrisse@gmail.com>
2026-02-07 15:38:38 +0100

    clean main

--- WPInstall.c
+++ WPInstall.c
@@ -16,45 +16,37 @@ int main() {
         NomDossier[strcspn(NomDossier, "\r\n")] = 0;
     }
     snprintf(CheminComplet, sizeof(CheminComplet), "C:/xampp/htdocs/Defi_Web/%s", NomDossier);
-    
-    goto bdd1;
-    
     if (_mkdir(CheminComplet) == 0) {
         printf("Succes : Le repertoire '%s' a ete cree.\n", CheminComplet);
         snprintf(Commande, sizeof(Commande), "copy \"%s\" \"%s\\wordpress.zip\" /Y > nul", SourceZip, CheminComplet);
         printf("Copie de l'archive en cours...\n");
         if (system(Commande) == 0) {
            printf("Succes : wordpress.zip a ete copie dans %s\n", CheminComplet);
-        } else {
-           printf("Erreur : Impossible de copier le fichier .zip. Verifiez le chemin source.\n");
-        }
-        printf("Extraction des fichiers en cours... Patientez...\n");
-        snprintf(Commande, sizeof(Commande), 
+           printf("Extraction des fichiers en cours... Patientez...\n");
+           snprintf(Commande, sizeof(Commande), 
                 "powershell -command \"Expand-Archive -Path '%s\\wordpress.zip' -DestinationPath '%s' -Force\"", 
                 CheminComplet, CheminComplet);
-        if (system(Commande) == 0) {
-            printf("Succes : wordpress a ete decompresse.");
-        }   else {
-            printf("Erreur : Impossible de decompresse wordpress.");
-            //goto EndProg;
-        }
-        printf(" Optimisation de la structure des dossiers...\n");
-        snprintf(Commande, sizeof(Commande), 
-            "powershell -command \"Get-ChildItem -Path '%s\\wordpress\\*' | Move-Item -Destination '%s\\' -Force\"", 
-             CheminComplet, CheminComplet);
-        if (system(Commande) == 0) {
-            printf("\nSucces total ! WordPress est pret dans : %s\n", CheminComplet);
-            snprintf(Commande, sizeof(Commande), "del \"%s\\wordpress.zip\"", CheminComplet);
-            system(Commande);
-            snprintf(Commande, sizeof(Commande), "rmdir \"%s\\wordpress\" > nul 2>&1", CheminComplet);
-            system(Commande);
-        } else {
+           if (system(Commande) == 0) {
+              printf("Succes : wordpress a ete decompresse.");
+              printf(" Optimisation de la structure des dossiers...\n");
+              snprintf(Commande, sizeof(Commande), 
+                "powershell -command \"Get-ChildItem -Path '%s\\wordpress\\*' | Move-Item -Destination '%s\\' -Force\"", 
+                CheminComplet, CheminComplet);
+              if (system(Commande) == 0) {
+                 printf("\nSucces total ! WordPress est pret dans : %s\n", CheminComplet);
+                 snprintf(Commande, sizeof(Commande), "del \"%s\\wordpress.zip\"", CheminComplet);
+                 system(Commande);
+                 snprintf(Commande, sizeof(Commande), "rmdir \"%s\\wordpress\" > nul 2>&1", CheminComplet);
+                 system(Commande);
+              } 
+            } else {
                 printf("Erreur lors de l'extraction.\n");
+            }    
         }
     } else {
         printf("Erreur lors de la creation du repertoire");
+        
     }
-    bdd1:
     printf("creation de la base de donnee\n");
     snprintf(Commande, sizeof(Commande), 
     "C:\\xampp\\mysql\\bin\\mysql.exe -u root -e \"CREATE DATABASE IF NOT EXISTS `%s`;\"", 
@@ -86,7 +78,7 @@ if (fSample && fConfig) {
         } else if (strstr(ligne, "password_here")) {
             fprintf(fConfig, "define( 'DB_PASSWORD', '' );\n", NomDossier);
         } else if (strstr(ligne, "$table_prefix = 'wp_';")) {
-            fprintf(fConfig, "$table_prefix = '%s_';\n", NomDossier);
+            fprintf(fConfig, "$table_prefix = 'az_';\n", NomDossier);
         } else {
             fputs(ligne, fConfig);
         }
@@ -101,9 +93,7 @@ if (fSample && fConfig) {
     char url[512];
     snprintf(url, sizeof(url), "start http://localhost/Defi_Web/%s", NomDossier);
     system(url);
-
-   // EndProg:
-        printf("\nAppuyez sur Entrer pour terminer...");
+    printf("\nAppuyez sur Entrer pour terminer...");
     getchar();
     return 0;
 }
